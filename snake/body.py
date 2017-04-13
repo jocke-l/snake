59cda4ce59cf2624181bb3e7262ed8a5e0b42632
Joakim Saario <saario.joakim@gmail.com>
2017-04-13 14:31:36 +0200

    Refactor, fix tests and make them pass

--- snake/body.py
+++ snake/body.py
@@ -1,4 +1,7 @@
 import enum
+from collections import namedtuple
+
+Coordinate = namedtuple('Coordinate', 'x y')
 
 
 class Direction(enum.Enum):
@@ -11,50 +14,43 @@ class Direction(enum.Enum):
         return abs(self.value)
 
 
-class Coordinate:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, other):
-        return self.x, self.y == other.x, other.y
-
-    def __str__(self):
-        return f'{self.__class__.__name__}({self.x}, {self.y})'
-
-
 class Segment:
-    def __init__(self, direction, start, length):
+    def __init__(self, direction, start, length, initial_forward=False):
         self.direction = direction
         self.start = start
         self.length = length
 
+        if initial_forward:
+            self.forward()
+
+    def __repr__(self):
+        return f'<{self}>'
+
+    def __str__(self):
+        return f'Segment({self.direction}, {self.start}, {self.length})'
+
     @property
     def end(self):
+        return self.forward_position(self.length - 1)
+
+    def forward_position(self, steps=1):
         if self.direction == Direction.RIGHT:
-            return Coordinate(self.start.x + self.length, self.start.y)
+            return Coordinate(self.start.x + steps, self.start.y)
         elif self.direction == Direction.LEFT:
-            return Coordinate(self.start.x - self.length, self.start.y)
+            return Coordinate(self.start.x - steps, self.start.y)
         elif self.direction == Direction.UP:
-            return Coordinate(self.start.x, self.start.y + self.length)
+            return Coordinate(self.start.x, self.start.y + steps)
         elif self.direction == Direction.DOWN:
-            return Coordinate(self.start.x, self.start.y - self.length)
+            return Coordinate(self.start.x, self.start.y - steps)
 
     def forward(self):
-        if self.direction == Direction.RIGHT:
-            self.start.x += 1
-        elif self.direction == Direction.LEFT:
-            self.start.x -= 1
-        elif self.direction == Direction.UP:
-            self.start.y += 1
-        elif self.direction == Direction.DOWN:
-            self.start.y -= 1
+        self.start = self.forward_position()
 
 
 class Body:
-    def __init__(self, initial_length):
-        self.segments = [Segment(Direction.RIGHT, Coordinate(3, 0),
-                                 initial_length)]
+    def __init__(self, head_segment_start, head_segment_length):
+        self.segments = [Segment(Direction.RIGHT, head_segment_start,
+                                 head_segment_length)]
 
     @property
     def length(self):
@@ -84,13 +80,17 @@ class Body:
             self.head_segment.forward()
         else:
             self.tail_segment.length -= 1
-            if self.tail_segment.length:
+            if self.tail_segment.length > 1:
                 self.tail_segment.forward()
             else:
+                # Merge last two segments
                 del self.tail_segment
+                self.tail_segment.start = self.tail_segment.forward_position(-1)
+                self.tail_segment.length += 1
 
             self.head_segment.length += 1
 
     def turn(self, direction):
         if abs(direction) != abs(self.head_segment.direction):
-            self.segments.append(Segment(direction, self.head_segment.start, 0))
+            self.segments.append(Segment(direction, self.head_segment.end, 0,
+                                         initial_forward=True))
--- snake/test_body.py
+++ snake/test_body.py
@@ -5,47 +5,102 @@ from snake.body import Body, Direction, Coordinate
 
 class TestBody(unittest.TestCase):
     def setUp(self):
-        self.body = Body(4)
+        self.body = Body(Coordinate(3, 5), 4)
 
     def test_initial(self):
+        #    y
+        #    7
+        #    6
+        #    5     x x x o
+        #    4
+        #    3
+        #    2
+        #    1
+        #  x   1 2 3 4 5 6 7
+
         self.assertEqual(len(self.body.segments), 1)
+        self.assertEqual(self.body.length, 4)
+        self.assertEqual(self.body.head_segment.start, Coordinate(3, 5))
+        self.assertEqual(self.body.head_segment.end, Coordinate(6, 5))
 
     def test_initial_forward(self):
         self.body.forward()
 
-        self.assertEqual(self.body.head_segment.start.x, 4)
+        #    y
+        #    7
+        #    6
+        #    5       x x x o
+        #    4
+        #    3
+        #    2
+        #    1
+        #  x   1 2 3 4 5 6 7 8
         self.assertEqual(self.body.length, 4)
+        self.assertEqual(len(self.body.segments), 1)
+        self.assertEqual(self.body.head_segment.start, Coordinate(4, 5))
+        self.assertEqual(self.body.head_segment.end, Coordinate(7, 5))
+        self.assertEqual(self.body.head_segment.length, 4)
 
     def test_forward_and_turn(self):
         self.body.turn(Direction.DOWN)
-
-        self.assertEqual(len(self.body.segments), 2)
         self.body.forward()
 
+        #    y
+        #    7
+        #    6
+        #    5       x x x
+        #    4           o
+        #    3
+        #    2
+        #    1
+        #  x   1 2 3 4 5 6 7 8
+        self.assertEqual(self.body.length, 4)
         self.assertEqual(len(self.body.segments), 2)
-        self.assertEqual(self.body.tail_segment.start, Coordinate(4, 0))
-        self.assertEqual(self.body.head_segment.end, Coordinate(7, -1))
+        self.assertEqual(self.body.tail_segment.start, Coordinate(4, 5))
+        self.assertEqual(self.body.tail_segment.end, Coordinate(6, 5))
+        self.assertEqual(self.body.tail_segment.length, 3)
+        self.assertEqual(self.body.head_segment.start, Coordinate(6, 4))
+        self.assertEqual(self.body.head_segment.end, Coordinate(6, 4))
         self.assertEqual(self.body.head_segment.length, 1)
 
         self.body.turn(Direction.LEFT)
         self.body.forward()
 
+        #    y
+        #    7
+        #    6
+        #    5         x x
+        #    4         o x
+        #    3
+        #    2
+        #    1
+        #  x   1 2 3 4 5 6 7 8
+        self.assertEqual(self.body.length, 4)
         self.assertEqual(len(self.body.segments), 3)
-        self.assertEqual(self.body.tail_segment.start, Coordinate(5, 0))
-        self.assertEqual(self.body.head_segment.end, Coordinate(6, -1))
+        self.assertEqual(self.body.tail_segment.start, Coordinate(5, 5))
+        self.assertEqual(self.body.tail_segment.end, Coordinate(6, 5))
+        self.assertEqual(self.body.tail_segment.length, 2)
+        self.assertEqual(self.body.segments[1].start, Coordinate(6, 4))
+        self.assertEqual(self.body.segments[1].end, Coordinate(6, 4))
+        self.assertEqual(self.body.segments[1].length, 1)
+        self.assertEqual(self.body.head_segment.start, Coordinate(5, 4))
+        self.assertEqual(self.body.head_segment.end, Coordinate(5, 4))
         self.assertEqual(self.body.head_segment.length, 1)
-        self.assertEqual(self.body.segments[-2].length, 1)
 
         self.body.forward()
         self.body.forward()
 
-        self.assertEqual(len(self.body.segments), 2)
-        self.assertEqual(self.body.tail_segment.start, Coordinate(6, -1))
-        self.assertEqual(self.body.head_segment.end, Coordinate(5, -1))
-        self.assertEqual(self.body.head_segment.length, 3)
-
-
-
-
-
-
+        #    y
+        #    7
+        #    6
+        #    5
+        #    4     o x x x
+        #    3
+        #    2
+        #    1
+        #  x   1 2 3 4 5 6 7 8
+        self.assertEqual(self.body.length, 4)
+        self.assertEqual(len(self.body.segments), 1)
+        self.assertEqual(self.body.head_segment.start, Coordinate(6, 4))
+        self.assertEqual(self.body.head_segment.end, Coordinate(3, 4))
+        self.assertEqual(self.body.head_segment.length, 4)
